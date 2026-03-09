28061241dc3813b0737752f7d80fd20c32fd38ba
lm88219-creator <lm88219@gmail.com>
2026-03-09 17:14:47 +0800

    UI upgrade

--- app/uu/dashboard/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-
-import { redirect } from "next/navigation";
-import { getDashboardEditPath } from "@/lib/routes";
-
-export default async function LegacyUuDashboardEditPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  redirect(getDashboardEditPath(id));
-}
--- app/uu/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-
-import { redirect } from "next/navigation";
-import { ROUTES } from "@/lib/routes";
-
-export default function LegacyUuLoginPage() {
-  redirect(ROUTES.login);
-}
